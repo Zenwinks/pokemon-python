1ed6a245a79289f9ffd97987c7cdfe7c0a9e292d
sfondain <steeve.fondain@gmail.com>
2020-01-09 08:36:19 +0100

    Commit test

--- pokemonPython/settings.py
+++ pokemonPython/settings.py
@@ -35,8 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pokemonApp.apps.PokemonappConfig',
-    'oauth2_provider'
+    'pokemonApp.apps.PokemonappConfig'
 ]
 
 MIDDLEWARE = [
